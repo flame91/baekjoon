89f95e4f394e4696bb41ffbf87290edb00245693
flame91 <52809844+flame91@users.noreply.github.com>
2020-06-23 13:07:51 +0900

    Update 1924.java

--- boj_number/1924.java
+++ boj_number/1924.java
@@ -1,3 +1,6 @@
+/*
+오늘은 2007년 1월 1일 월요일이다. 그렇다면 2007년 x월 y일은 무슨 요일일까? 이를 알아내는 프로그램을 작성하시오.
+*/
 import java.util.Scanner;
 
 public class Main {
@@ -7,7 +10,7 @@ public class Main {
 		int month = sc.nextInt();
 		int day = sc.nextInt();
 		
-		int[] cal = {31,28,31,30,31,30,31,31,30,31,30,31};
+		int[] cal = {31,28,31,30,31,30,31,31,30,31,30,31}; // 월별 일수
 		String[] date = {"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"};
 		int cnt=0;
 		
