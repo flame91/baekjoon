3ffa638c742edb5ffeca08a3972acabc9d467adb
flame91 <52809844+flame91@users.noreply.github.com>
2020-06-23 12:58:16 +0900

    Update 1001.cpp

--- boj_number/1001.cpp
+++ boj_number/1001.cpp
@@ -1,6 +1,10 @@
+/*
+두 정수 A와 B를 입력받은 다음, A-B를 출력하는 프로그램을 작성하시오.
+*/
+
 #include <stdio.h>
 int a,b;
 int main(){
-    scanf("%d %d",&a,&b);
-    printf("%d\n",a-b);
+    scanf("%d %d",&a,&b); // 두 정수를 입력받아
+    printf("%d\n",a-b); // a에서 b를 뺀 값을 출력
 }
