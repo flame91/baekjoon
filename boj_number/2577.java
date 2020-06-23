006ece169088800d52f46841b297c8c2e8a24a86
flame91 <52809844+flame91@users.noreply.github.com>
2020-06-23 13:17:32 +0900

    Update 2577.java

--- boj_number/2577.java
+++ boj_number/2577.java
@@ -1,3 +1,12 @@
+/*
+세 개의 자연수 A, B, C가 주어질 때 A×B×C를 계산한 결과에 0부터 9까지 각각의 숫자가 몇 번씩 쓰였는지를 구하는 프로그램을 작성하시오.
+
+예를 들어 A = 150, B = 266, C = 427 이라면 
+
+A × B × C = 150 × 266 × 427 = 17037300 이 되고, 
+
+계산한 결과 17037300 에는 0이 3번, 1이 1번, 3이 2번, 7이 2번 쓰였다.
+*/
 import java.util.Scanner;
 
 public class Main {
@@ -10,7 +19,7 @@ public class Main {
 		
 		int product = a*b*c;
 		
-		int[] arr = new int[10];
+		int[] arr = new int[10]; //1부터 10의자리, 각각 사용된 횟수
 		
 		while(true)
 		{
@@ -19,7 +28,7 @@ public class Main {
 			else
 			{
 				arr[product%10]++;
-				product /= 10;
+				product /= 10; // 한 자리수 날리기
 			}
 		}
 		
