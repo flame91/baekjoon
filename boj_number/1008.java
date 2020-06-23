aafb5286c85dbc5eaa3c7c682cc08da9753d7c2e
flame91 <52809844+flame91@users.noreply.github.com>
2020-06-23 12:59:07 +0900

    Update 1008.java

--- boj_number/1008.java
+++ boj_number/1008.java
@@ -1,13 +1,18 @@
+/*
+두 정수 A와 B를 입력받은 다음, A/B를 출력하는 프로그램을 작성하시오.
+*/
+
 import java.util.*;
  
 public class Main {
     public static void main(String[] args){
         
+     // 두 정수 입력
          Scanner scan = new Scanner(System.in);
          double num1 = scan.nextInt();
          double num2 = scan.nextInt();
          
-         System.out.println(num1/num2);
+         System.out.println(num1/num2); // num1을 num2로 나눈 값 출력
         
     }
 }
