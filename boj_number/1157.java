4a717664a033021f147004887676b43c8814c172
flame91 <52809844+flame91@users.noreply.github.com>
2020-06-23 13:04:43 +0900

    Update 1157.java

--- boj_number/1157.java
+++ boj_number/1157.java
@@ -1,3 +1,8 @@
+/*
+알파벳 대소문자로 된 단어가 주어지면,
+이 단어에서 가장 많이 사용된 알파벳이 무엇인지 알아내는 프로그램을 작성하시오.
+단, 대문자와 소문자를 구분하지 않는다.
+*/
 import java.util.Scanner;
 
 public class Main {
@@ -5,7 +10,7 @@ public class Main {
 		
 		Scanner sc = new Scanner(System.in);
 		String str = sc.next();
-		int[] alp = new int[26];
+		int[] alp = new int[26]; // 어느 알파벳이 많이 쓰였는지 카운트용 변수
 		int max=0, max_num=0, cnt=0;
 		
 		for(int i=0; i<str.length(); i++) { // 아스키코드 65~90 대문자, 97~122//소문자;
@@ -20,7 +25,7 @@ public class Main {
 		
 		for(int i=0; i<26; i++) {
 			if(alp[i]>max) {
-				max=alp[i]; max_num=i;
+				max=alp[i]; max_num=i; // 가장 많이 쓰인 알파벳 구하기
 			}
 		}
 		
