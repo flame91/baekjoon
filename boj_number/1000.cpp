cf6cdf586f0e65fcf9a4e9a039585f20359a7bfb
flame91 <52809844+flame91@users.noreply.github.com>
2020-06-23 12:57:38 +0900

    Update 1000.cpp

--- boj_number/1000.cpp
+++ boj_number/1000.cpp
@@ -1,9 +1,13 @@
+/*
+두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+*/
+
 #include <stdio.h>
 
 int a,b;
 
 int main(){
-    scanf("%d %d",&a, &b);
+    scanf("%d %d",&a, &b); // 입력부
     
-    printf("%d\n",a+b);
+    printf("%d\n",a+b); // 출력부
 }
